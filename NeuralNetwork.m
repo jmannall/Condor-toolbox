3046decbe83146d9325ffd363483759552817975
jmannall <110119245+jmannall@users.noreply.github.com>
2022-10-29 12:04:35 +0100

    Neural network script

--- NeuralNetwork.m
+++ NeuralNetwork.m
@@ -1,35 +1,39 @@
-clc;
-clear;
-close all;
-set(0, 'DefaultLineLineWidth', 2);
-set(groot, 'defaultLineMarkerSize', 10)
+function NeuralNetwork()
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    clc;
+    clear;
+    close all;
+    set(0, 'DefaultLineLineWidth', 2);
+    set(groot, 'defaultLineMarkerSize', 10)
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %% NN
+    
+    % Hyperparamters
+    % learnRate = 1e-2;
+    % gradDecay = 0.9;
+    % sqGradDecay = 0.999;
+    % maxGradient = 5;
+    
+    % x.lR = learnRate;
+    % x.gD = gradDecay;
+    % x.sGD = sqGradDecay;
+    % x.mG = maxGradient;
+    % loss = XORNeuralNetwork(x);
+    
+    learnRate = optimizableVariable('lR',[1e-5 1e-1],'Type','real','Transform','log');
+    gradDecay = optimizableVariable('gD',[0.5 1],'Type','real');
+    sqGradDecay = optimizableVariable('sGD',[0.8 1],'Type','real');
+    maxGradient = optimizableVariable('mG',[0.5 10],'Type','real','Transform','log');
+    
+    func = @(x)XORNeuralNetwork(x);
+    result = bayesopt(func, [learnRate, gradDecay, sqGradDecay, maxGradient], 'UseParallel', true);
+    
+    xObs = result.XAtMinObjective;
+    xEst = result.XAtMinEstimatedObjective;
+    
+    [lossObs, netObs] = XORNeuralNetwork(xObs)
+    [lossEst, netEst] = XORNeuralNetwork(xEst)
 
-%% NN
-
-% Hyperparamters
-% learnRate = 1e-2;
-% gradDecay = 0.9;
-% sqGradDecay = 0.999;
-% maxGradient = 5;
-
-% x.lR = learnRate;
-% x.gD = gradDecay;
-% x.sGD = sqGradDecay;
-% x.mG = maxGradient;
-% loss = XORNeuralNetwork(x);
-
-learnRate = optimizableVariable('lR',[1e-5 1e-1],'Type','real','Transform','log');
-gradDecay = optimizableVariable('gD',[0.5 1],'Type','real');
-sqGradDecay = optimizableVariable('sGD',[0.8 1],'Type','real');
-maxGradient = optimizableVariable('mG',[0.5 10],'Type','real','Transform','log');
-
-func = @(x)XORNeuralNetwork(x);
-result = bayesopt(func, [learnRate, gradDecay, sqGradDecay, maxGradient], 'UseParallel', true);
-
-xObs = result.XAtMinObjective;
-xEst = result.XAtMinEstimatedObjective;
-
-[lossObs, netObs] = XORNeuralNetwork(xObs)
-[lossEst, netEst] = XORNeuralNetwork(xEst)
+end
--- myscript.m
+++ myscript.m
@@ -1,6 +1,4 @@
 
 addpath('/mnt/fast/nobackup/users/jm01527/scripts')
-%addpath(genpath('C:/Users/jm01527/Docker/test'))
-currentFolder = pwd
-path
-HelloWorld()
+
+NeuralNetwork()
