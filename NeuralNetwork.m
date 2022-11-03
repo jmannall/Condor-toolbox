694eb281602d82490b03ee78292746ba431e8858
jmannall <110119245+jmannall@users.noreply.github.com>
2022-11-03 15:01:39 +0000

    Add func not as passed arg

--- NeuralNetwork.m
+++ NeuralNetwork.m
@@ -9,18 +9,22 @@ function NeuralNetwork(networkFunction)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %% NN
-    
+    disp('Check 1');
     learnRate = optimizableVariable('lR',[1e-5 1e-1],'Type','real','Transform','log');
     gradDecay = optimizableVariable('gD',[0.5 1],'Type','real');
     sqGradDecay = optimizableVariable('sGD',[0.8 1],'Type','real');
     maxGradient = optimizableVariable('mG',[0.5 10],'Type','real','Transform','log');
     
+    networkFunction = @(x)XORNeuralNetwork(x);
     result = bayesopt(networkFunction, [learnRate, gradDecay, sqGradDecay, maxGradient], 'UseParallel', true);
     
     xObs = result.XAtMinObjective;
     xEst = result.XAtMinEstimatedObjective;
 
-    [lossObs, netObs] = XORNeuralNetwork(xObs)
-    [lossEst, netEst] = XORNeuralNetwork(xEst)
+    disp('Check 2')
+    [lossObs, netObs] = networkFunction(xObs)
+    [lossEst, netEst] = networkFunction(xEst)
+    disp('ParamsObs', num2str(netObs))
+    disp('ParamsEst', num2str(netEst))
 
 end
