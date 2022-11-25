2f75b0c386cfa64936e0994cc6699007c826564c
jmannall <110119245+jmannall@users.noreply.github.com>
2022-11-25 20:49:48 +0000

    Final test run set up

--- BayesoptScript.m
+++ BayesoptScript.m
@@ -9,35 +9,45 @@ close all;
 %addpath(genpath([basePath, 'Edge-diffraction-Matlab-toolbox']))
 %addpath(genpath([basePath, 'misc-files']))
 
-% Create controlparamters struct
+%% Set up data
 fs = 96e3;
 nfft = 16384;
 c = 344;
 controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
 
-% Create loss function
 numFilters = 2;
 nBands = 12;
 [~, tfmag, ~, fvec, ~] = DefaultBTM(controlparameters);
 [~, ~, fidx] = CreateFrequencyNBands(tfmag, fvec, nBands);
-biquad = true;
-if biquad
-    filterFunc = @(output, target) BiquadLoss(output, target, numFilters, nfft, fs, fidx);
-    numOutputs = 4 * numFilters + 1;
-    controlparameters.filterType = 'Biquad';
-else
-    filterFunc = @(output, target) IIRFilterLoss(output, target, numFilters, nfft, fs, fidx);
-    numOutputs = 2 * numFilters + 1;
-    controlparameters.filterType = 'IIR';
-end
+
+%% Biquads
+filterFunc = @(output, target) BiquadLoss(output, target, numFilters, nfft, fs, fidx);
+numOutputs = 4 * numFilters + 1;
+controlparameters.filterType = 'Biquad';
+
 lossFunc = @(net, trainingData, targetData) NNFilterLoss(net, trainingData, targetData, filterFunc, true);
 
 % Optimise hyperparameters
+disp('Biquad - 700')
 networkSize = 700;
 BayesoptNeuralNetwork(lossFunc, networkSize, numOutputs, controlparameters)
 
-networkSize = 10e3;
+disp('Biquad - 350')
+networkSize = 350;
 BayesoptNeuralNetwork(lossFunc, networkSize, numOutputs, controlparameters)
 
+%% IIR filters
+filterFunc = @(output, target) IIRFilterLoss(output, target, numFilters, nfft, fs, fidx);
+numOutputs = 2 * numFilters + 1;
+controlparameters.filterType = 'IIR';
+
+lossFunc = @(net, trainingData, targetData) NNFilterLoss(net, trainingData, targetData, filterFunc, true);
+
+% Optimise hyperparameters
+disp('IIR - 700')
+networkSize = 700;
+BayesoptNeuralNetwork(lossFunc, networkSize, numOutputs, controlparameters)
+
+disp('IIR - 350')
 networkSize = 350;
 BayesoptNeuralNetwork(lossFunc, networkSize, numOutputs, controlparameters)
