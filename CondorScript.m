b620557e49cc6ac193b777a5fccff16da2e687d0
jmannall <110119245+jmannall@users.noreply.github.com>
2022-11-30 21:47:11 +0000

    Add COndorScript

---
+++ CondorScript.m
@@ -0,0 +1,16 @@
+close all
+
+numEpochs = 500;
+epochSize = 20e3;
+
+fs = 96e3;
+nfft = 16384;
+c = 344;
+controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
+
+% gpuDevice(1)
+rng(4000)
+
+for i = 350:numEpochs
+    [trainingData, targetData] = CreateBtmTrainingData(epochSize, controlparameters, i);
+end
