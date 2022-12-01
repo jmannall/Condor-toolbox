050060113aeffec042b8791a30cdadf1b9ba76c2
jmannall <110119245+jmannall@users.noreply.github.com>
2022-12-01 13:16:31 +0000

    Test save locations

--- CondorScript.m
+++ CondorScript.m
@@ -4,17 +4,14 @@ function CondorScript(idx)
     disp(idx)
     disp('Hello World')
 
-    fs = 96e3;
-    nfft = 16384;
-    c = 344;
     controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 1);
-
-    tf = SingleWedgeInterpolated(20, 350, 10, 260, 1, 2, 2, 11, controlparameters, false);
+    tf = SingleWedge(20, 350, 10, 290, 2, 4, 9, 8, controlparameters, false);
 
 %     poolobj = gcp
 %     if isempty(poolobj)
 %         parpool([1 48]);
 %     end
+% 
 %     epochSize = 20e3;
 %     
 %     fs = 96e3;
