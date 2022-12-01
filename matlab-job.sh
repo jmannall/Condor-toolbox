9a62c5728bec63b7d92da60ea9cca67e939fd537
jmannall <110119245+jmannall@users.noreply.github.com>
2022-12-01 13:00:16 +0000

    Test file transfer

--- matlab-job.sh
+++ matlab-job.sh
@@ -2,3 +2,5 @@
 git clone https://github.com/jmannall/Diffraction-toolbox.git
 git clone https://github.com/jmannall/Edge-diffraction-Matlab-toolbox.git
 exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); addpath(genpath('Diffraction-toolbox')); addpath(genpath('Edge-diffraction-Matlab-toolbox')); run('CondorScript($1)'); quit"
+rm -f -r Diffraction-toolbox
+rm -f -r Edge-diffraction-Matlab-toolbox
