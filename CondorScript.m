680e99273c451954247ecaf1cb5085620dde8c97
jmannall <110119245+jmannall@users.noreply.github.com>
2022-12-01 12:44:14 +0000

    Test git clone

--- CondorScript.m
+++ CondorScript.m
@@ -1,27 +1,29 @@
 
 function CondorScript(idx)
 
-    poolobj = gcp
-    if isempty(poolobj)
-        parpool([1 48]);
-    end
-    epochSize = 20e3;
-    
-    fs = 96e3;
-    nfft = 16384;
-    c = 344;
-    controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
-    
-    % gpuDevice(1)
-    seed = 1e3 * idx;
-    rng(seed)
-    
-    seed = round(2 ^ 32 * rand(1)) - 1
-    rng(seed)
-    
-    numDataSets = 10;
-    for i = 1:numDataSets
-        idx = numDataSets * idx + i;
-        [trainingData, targetData] = CreateBtmTrainingData(epochSize, controlparameters, numDataSets * idx + i);
-    end
+    disp(idx)
+    disp('Hello World')
+%     poolobj = gcp
+%     if isempty(poolobj)
+%         parpool([1 48]);
+%     end
+%     epochSize = 20e3;
+%     
+%     fs = 96e3;
+%     nfft = 16384;
+%     c = 344;
+%     controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
+%     
+%     % gpuDevice(1)
+%     seed = 1e3 * idx;
+%     rng(seed)
+%     
+%     seed = round(2 ^ 32 * rand(1)) - 1
+%     rng(seed)
+%     
+%     numDataSets = 10;
+%     for i = 1:numDataSets
+%         idx = numDataSets * idx + i;
+%         [trainingData, targetData] = CreateBtmTrainingData(epochSize, controlparameters, numDataSets * idx + i);
+%     end
 end
--- matlab-job.sh
+++ matlab-job.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527')); run('CondorScript($1)'); quit"
+git clone https://github.com/jmannall/Diffraction-toolbox.git
+git clone https://github.com/jmannall/Edge-diffraction-Matlab-toolbox.git
+exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); addpath(genpath('Diffraction-toolbox')); addpath(genpath('Edge-diffraction-Matlab-toolbox')); run('CondorScript($1)'); quit"
