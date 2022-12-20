c8b46d8176eb6b9f7e284fde31e51de05d12d7b1
jmannall <110119245+jmannall@users.noreply.github.com>
2022-12-20 11:20:17 +0000

    Generate data for 48 and 44.1 kHz training

--- CondorScript.m
+++ CondorScript.m
@@ -28,11 +28,20 @@ function CondorScript(idx)
     seed = round(2 ^ 32 * rand(1)) - 1
     rng(seed)
     
-    numDataSets = 10;
+    numDataSets = 5;
     
     weight = 20;
     for i = 1:numDataSets
-        [trainingData, targetData] = CreateBtmTrainingData(epochSize, controlparameters, epochSize);
-        [trainingData, targetData] = CreateBtmTrainingDataWeighted(epochSize, controlparameters, weight, epochSize);
+        saveIdx = idx * numDataSets + i;
+        [~, ~] = CreateBtmTrainingData(epochSize, controlparameters, saveIdx);
+        [~, ~] = CreateBtmTrainingDataWeighted(epochSize, controlparameters, weight, saveIdx);
+    end
+
+    fs = 88.2e3;
+    controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
+    for i = 1:numDataSets
+        saveIdx = idx * numDataSets + i;
+        [~, ~] = CreateBtmTrainingData(epochSize, controlparameters, saveIdx);
+        [~, ~] = CreateBtmTrainingDataWeighted(epochSize, controlparameters, weight, saveIdx);
     end
 end
