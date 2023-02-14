cc6f98956a13c413150649d4979a0701a0037fac
jmannall <110119245+jmannall@users.noreply.github.com>
2023-02-14 15:11:28 +0000

    GenerateAudioScripts

---
+++ matlab-audio-job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/jmannall/Diffraction-toolbox.git
+git clone https://github.com/jmannall/Edge-diffraction-Matlab-toolbox.git
+exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); addpath(genpath('Diffraction-toolbox')); addpath(genpath('Edge-diffraction-Matlab-toolbox')); run('GenerateAudio($1)'); quit"
+rm -f -r Diffraction-toolbox
+rm -f -r Edge-diffraction-Matlab-toolbox
