ef64dfc1b564adc1d7c6caff8ce80ea5985864bd
jmannall <110119245+jmannall@users.noreply.github.com>
2023-02-15 13:16:31 +0000

    Input fileName as variable

--- matlab-audio-job.sh
+++ matlab-audio-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/jmannall/Diffraction-toolbox.git
 git clone https://github.com/jmannall/Edge-diffraction-Matlab-toolbox.git
-exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); addpath(genpath('Diffraction-toolbox')); addpath(genpath('Edge-diffraction-Matlab-toolbox')); run('GenerateAudio(char("$1"), $2)'); quit"
+exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); addpath(genpath('Diffraction-toolbox')); addpath(genpath('Edge-diffraction-Matlab-toolbox')); fileName = '$1'; run('GenerateAudio(fileName, $2 + 1)'); quit"
 rm -f -r Diffraction-toolbox
 rm -f -r Edge-diffraction-Matlab-toolbox
