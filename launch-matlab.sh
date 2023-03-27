92429c4aa0387bc55d351b66fd49ffc9f9e4b278
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-27 17:37:19 +0100

    Try lauch-matlab with arguments

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -nodisplay -nosplash -r \
+exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -nodisplay -nosplash -batch \
 "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
 addpath(genpath('Diffraction-toolbox')); \
-addpath(genpath('Edge-diffraction-Matlab-toolbox'));" \
+addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
+z = dlarray($1 + $2i); \
+r = real(z); \
+im = imag(z); \
+disp(['Complex: ' num2str(z)]); \
+disp(['Real: ' num2str(r)]); \
+disp(['Imaginary: ' num2str(im)]);"
--- matlab-test-job.sh
+++ matlab-test-job.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 sh ./git-clone.sh
-sh ./launch-matlab.sh
-"z = dlarray(2 + 3i); \
-r = real(z); \
-im = imag(z); \
-disp(['Complex: ' num2str(z)]); \
-disp(['Real: ' num2str(r)]); \
-disp(['Imaginary: ' num2str(im)]); \
-quit;"
+sh ./launch-matlab.sh -1 -4
 sh ./rm-folders.sh
