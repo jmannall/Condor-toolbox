b3619235dfe8f12dd65d132b4a29d87178b88889
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-27 17:51:35 +0100

    Test run script

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -nodisplay -nosplash -batch \
+exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -batch \
 "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
 addpath(genpath('Diffraction-toolbox')); \
 addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
-z = dlarray($1 + $2i); \
-r = real(z); \
-im = imag(z); \
-disp(['Complex: ' num2str(z)]); \
-disp(['Real: ' num2str(r)]); \
-disp(['Imaginary: ' num2str(im)]);"
+$1;"
