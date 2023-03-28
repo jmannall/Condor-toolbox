d144edd628c159cfdd8d0ae33f74b5483d5f2891
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-28 17:23:44 +0100

    Switch to $

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set message=Hello World 
-echo %message%
+echo $message
 
 set message = $1'('
-echo %message
+echo $message
 
 set /A ARGS_COUNT = 0
 
-for %x in (%*) do set /A ARGS_COUNT += 1    
-echo %ARGS_COUNT%
+for $x in ($*) do set /A ARGS_COUNT += 1    
+echo $ARGS_COUNT
 
-for %x in (2, 1, ARGS_COUNT) do set message = message %1,
+for $x in (2, 1, ARGS_COUNT) do set message = message $x,
 
 set message = message')'
-echo %message%
+echo $message
 
 exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -batch \
 "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
 addpath(genpath('Diffraction-toolbox')); \
 addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
-message;"
+$message;"
