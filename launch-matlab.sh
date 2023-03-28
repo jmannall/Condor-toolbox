d69824dc74fe9e485e6de8c6f6c4ce5af68ff4f8
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-28 18:56:49 +0100

    Manual input argument cases

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -7,19 +7,29 @@ echo $counter
 number="$@"
 echo $number
 
-ARGS=1
-if [ $# -eq "$ARGS" ]
+if [ $# -eq "0" ]
 then
-echo No arguments
-message="$1"
+    echo No input arguments
+elif [ $# -eq "1" ]
+then
+    message="$1"
+elif [ $# -eq "2" ]
+then
+    message="$1($2)"
+elif [ $# -eq "3" ]
+then
+    message="$1($2, $3)"
+elif [ $# -eq "4" ]
+then
+    message="$1($2, $3, $4)"
+elif [ $# -eq "5" ]
+then
+    message="$1($2, $3, $4, $5)"
+elif [ $# -eq "6" ]
+then
+    message="$1($2, $3, $4, $5, $6)"
 else
-echo Loop through arguments
-message="$1($2"
-for value in {2..$#}
-do
-message="$message, $value"
-done
-message="$message)"
+    echo More than five input arguments
 fi
 
 echo $message
