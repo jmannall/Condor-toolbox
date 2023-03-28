4685ffaa3dc6ebe6f860f6f79d8897b622af273b
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-28 17:06:36 +0100

    Next attempt

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-@echo off 
 set message = $1'('
 echo %message%
 
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+echo %argC%
+
 set /A ARGS_COUNT = 0    
-for %%A in (%*) do set /A ARGS_COUNT += 1    
+for %%x in (%*) do set /A ARGS_COUNT += 1    
 echo %ARGS_COUNT%
 
 for %%x IN (2, 1, ARGS_COUNT) do set message = message %1,
