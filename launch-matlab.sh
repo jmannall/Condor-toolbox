9652d4223f8dbe50b1c5f6b358e590603e18f5cd
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-28 17:02:44 +0100

    Attempt argument inputs

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+@echo off 
+set message = $1'('
+echo %message%
+
+set /A ARGS_COUNT = 0    
+for %%A in (%*) do set /A ARGS_COUNT += 1    
+echo %ARGS_COUNT%
+
+for %%x IN (2, 1, ARGS_COUNT) do set message = message %1,
+
+set message = message')'
+echo %message%
+
 exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -batch \
 "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
 addpath(genpath('Diffraction-toolbox')); \
 addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
-$1;"
+message;"
