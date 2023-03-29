cfd5ba0d336c84b721dd0e3c55e13dc80cd5b278
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 18:30:17 +0100

    pass $4 as string

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sh ./git-clone.sh
-a=$(($3 / 10**4))
-echo $a
-sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $a $4
+sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $3 "'$4'"
 sh ./rm-folders.sh
