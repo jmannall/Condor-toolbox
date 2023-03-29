ea7e2dd342a65c33417ce39ff8d1ba879768e2e7
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 18:03:29 +0100

    arithmetic as variable

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sh ./git-clone.sh
-sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $($3 * 10 ^ (-4)) $4
+a = $(($3 * 10 ^ (-4))
+sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $a $4
 sh ./rm-folders.sh
