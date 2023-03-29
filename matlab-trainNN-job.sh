7dcfb0562df290569b7418952d9980d968ed7389
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 19:07:26 +0100

    Add else

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -2,13 +2,17 @@
 sh ./git-clone.sh
 if [ $3 -eq "0" ]
 then
-learnRate=$((0.001))
+learnRate=$((1000))
 elif [ $3 -eq "1" ]
 then
-learnRate=$((0.0001))
+learnRate=$((10000))
 elif [ $3 -eq "2" ]
 then
-learnRate=$((0.00001))
+learnRate=$((100000))
+else
+learnRate=$((1))
+echo Learn rate set to default
 fi
+echo $learnRate
 sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $learnRate "'$4'"
 sh ./rm-folders.sh
