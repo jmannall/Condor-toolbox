8a93fc00bb0a44e179a5a7b719421d8dc5e953fa
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 19:19:07 +0100

    Retry if statement

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ $3 -eq "0" ]
+then
+learnRate=$((1000))
+elif [ $3 -eq "1" ]
+learnRate=$((10000))
+else
+learnRate=$((1000))
+echo Learn rate set to default
+fi
 sh ./git-clone.sh
 sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $3 "'$4'"
 sh ./rm-folders.sh
