8eb24f0b1452bf4a9c39a96e55b8ee28b7c38f36
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 19:04:41 +0100

    Create learnRate based on if statement

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sh ./git-clone.sh
-sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $3 "'$4'"
+if [ $3 -eq "0" ]
+then
+learnRate=$((0.001))
+elif [ $3 -eq "1" ]
+then
+learnRate=$((0.0001))
+elif [ $3 -eq "2" ]
+then
+learnRate=$((0.00001))
+fi
+sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $learnRate "'$4'"
 sh ./rm-folders.sh
