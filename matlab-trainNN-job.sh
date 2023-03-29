f7711e0d482265cd52968af735e36be437811fd8
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 19:12:09 +0100

    Remove comments

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 sh ./git-clone.sh
-#if [ $3 -eq "0" ]
-#then
-#learnRate=$((1000))
-#elif [ $3 -eq "1" ]
-#then
-#learnRate=$((10000))
-#elif [ $3 -eq "2" ]
-#then
-#learnRate=$((100000))
-#else
-#learnRate=$((1))
-#echo Learn rate set to default
-#fi
-#$learnRate=$((100))
-#echo $learnRate
 sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $3 "'$4'"
 sh ./rm-folders.sh
