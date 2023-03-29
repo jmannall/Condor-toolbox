afbd4e4a4370456b6d16391c5da43874599cc253
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 18:15:40 +0100

    external '

--- matlab-trainNN-job.sh
+++ matlab-trainNN-job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh ./git-clone.sh
-a = $(($3 * 1 / 10**4))
-sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $a $4
+#a = $(($3 * 1 / 10**4))
+sh ./launch-matlab.sh TrainNeuralNetwork $1 $2 $3 $4
 sh ./rm-folders.sh
