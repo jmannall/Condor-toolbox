25daefca2fc32bd40c7b2390692187b18641566b
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 10:52:52 +0100

    Back to 2022a

--- launch-matlab.sh
+++ launch-matlab.sh
@@ -30,7 +30,7 @@ fi
 echo $message
 
 echo Launch MATLAB
-exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -batch \
+exec /vol/hpc-software/software/matlab/R2022a/bin/matlab -batch \
 "addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
 addpath(genpath('Diffraction-toolbox')); \
 addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
