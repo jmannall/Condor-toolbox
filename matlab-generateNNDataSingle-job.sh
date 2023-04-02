423353cc0247eabc23fbfbefe93f46f264e0e60c
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-02 11:19:59 +0100

    Re run two corrupted data sets

--- matlab-generateNNDataSingle-job.sh
+++ matlab-generateNNDataSingle-job.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash]
 sh ./git-clone.sh
+if [ $1 -eq "0" ]
+then
+idx=$((397))
+sh ./launch-matlab.sh GenerateNNData $idx $2
+elif [ $1 -eq "1" ]
+then
+idx=$((486))
+sh ./launch-matlab.sh GenerateNNData $idx $2
+else
 sh ./launch-matlab.sh GenerateNNDataSingle $1 $2
+fi
 sh ./rm-folders.sh
