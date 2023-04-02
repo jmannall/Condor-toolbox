6fe7e4065e70c03a604b96f8c151a50ae954acf8
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-02 20:50:34 +0100

    Return generate scripts to intended use

--- matlab-generateNNDataSingle-job.sh
+++ matlab-generateNNDataSingle-job.sh
@@ -1,16 +1,4 @@
 #!/bin/bash]
 sh ./git-clone.sh
-#if [ $1 -eq "0" ]
-#then
-#idx=$((397))
-#sh ./launch-matlab.sh GenerateNNData $idx $2
-#elif [ $1 -eq "1" ]
-#then
-#idx=$((486))
-#sh ./launch-matlab.sh GenerateNNData $idx $2
-#else
-#sh ./launch-matlab.sh GenerateNNDataSingle $1 $2
-#fi
-idx=$((397))
-sh ./launch-matlab.sh GenerateNNData $idx $2
+sh ./launch-matlab.sh GenerateNNDataSingle $1 $2
 sh ./rm-folders.sh
