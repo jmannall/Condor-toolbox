01b092b04b1da4a812b6a6b567b25897c515eca4
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-06 17:52:21 +0100

    Run missing data sets

--- matlab-generateNNData-job.sh
+++ matlab-generateNNData-job.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ $1 -eq "0" ]
+then
+num=$((28))
+elif [ $1 -eq "1" ]
+then
+num=$((37))
+fi
 sh ./git-clone.sh
-sh ./launch-matlab.sh GenerateNNData $1 $2
+sh ./launch-matlab.sh GenerateNNData $num $2
 sh ./rm-folders.sh
