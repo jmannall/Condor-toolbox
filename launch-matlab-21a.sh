79d2301dd34924c664c275979e05c37ca1fe7f3d
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-19 15:08:35 +0100

    GenerateData using 2021a

---
+++ launch-matlab-21a.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+hello="Hello World"
+echo $hello
+
+if [ $# -eq "0" ]
+then
+    echo No input arguments
+elif [ $# -eq "1" ]
+then
+    message="$1"
+elif [ $# -eq "2" ]
+then
+    message="$1($2)"
+elif [ $# -eq "3" ]
+then
+    message="$1($2, $3)"
+elif [ $# -eq "4" ]
+then
+    message="$1($2, $3, $4)"
+elif [ $# -eq "5" ]
+then
+    message="$1($2, $3, $4, $5)"
+elif [ $# -eq "6" ]
+then
+    message="$1($2, $3, $4, $5, $6)"
+else
+    echo More than five input arguments
+fi
+
+echo $message
+
+echo Launch MATLAB
+exec /vol/hpc-software/software/matlab/R2021a/bin/matlab -batch \
+"addpath(genpath('/mnt/fast/nobackup/users/jm01527/misc-files')); \
+addpath(genpath('Diffraction-toolbox')); \
+addpath(genpath('Edge-diffraction-Matlab-toolbox')); \
+$message;"
--- matlab-generateNNData-job.sh
+++ matlab-generateNNData-job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sh ./git-clone.sh
-sh ./launch-matlab.sh GenerateNNData $1 $2
+sh ./launch-matlab-21a.sh GenerateNNData $1 $2
 sh ./rm-folders.sh
